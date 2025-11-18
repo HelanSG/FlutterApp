eadb336a5199b10d1553e29b73a03e4043d7df5d
walber.beltrame <walber.beltrame@gmail.com>
2025-11-18 12:06:25 +0000

    Semana 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'components.dart';
+import 'students_page.dart';
 import 'theme.dart';
 
 void main() {
@@ -124,6 +125,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (_) => const ComponentsPage()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Alunos'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const StudentsPage()),
                   );
                 },
               ),
